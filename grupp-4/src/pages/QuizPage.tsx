c07d2381e9e4d0f94eb769417a5d98925a667f22
sasiie <Sarchiradic99@gmail.com>
2026-01-25 22:46:51 +0100

    Tog bort kommentar

--- grupp-4/src/pages/QuizPage.tsx
+++ grupp-4/src/pages/QuizPage.tsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import type { Question } from "../types/quiz";
 import type { QuizCategory } from "../data/questions";
 import { getHighscore, setHighscore } from "../data/storage";
-//import { questions as allQuestions } from "../data/questions";
 import { useParams, useNavigate } from "react-router-dom";
 import { questions as allQuizzes } from "../data/questions";
 
